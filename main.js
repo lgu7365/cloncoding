3fc8e7ef36c848c6269d4db919bd1403e46b2955
Leegeonu <lgu_lgu@naver.com>
2022-11-19 12:40:23 +0900

    ad-slider section 반응형

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ const adsliderprev = document.querySelector('.prev');
 const adslidernext = document.querySelector('.next');
 const peopleprev = document.querySelector('.prev2');
 const peoplenext = document.querySelector('.next2');
+const msdot1 = document.querySelector('.ms-dot1');
+const msdot2 = document.querySelector('.ms-dot2');
+const msdot3 = document.querySelector('.ms-dot3');
+const msdot4 = document.querySelector('.ms-dot4');
+const msdot5 = document.querySelector('.ms-dot5');
 const mdot1 = document.querySelector('.m-dot1');
 const mdot2 = document.querySelector('.m-dot2');
 const mdot3 = document.querySelector('.m-dot3');
 const idot1 = document.querySelector('.i-dot1');
 const idot2 = document.querySelector('.i-dot2');
-const idot3 = document.querySelector('.i-dot3')
+const idot3 = document.querySelector('.i-dot3');
+
 
 let translate = 0;
 let translatefa = -4270;
@@ -41,27 +47,71 @@ adslidernext.addEventListener('click', () => {
 //   document.querySelector('.slider-list').style.transform = `translate(${translate-100}vw)`;
 //   translate = translate - 100;
 // })
-const cntfa = document.querySelector('.people').childElementCount;
+// const cntfa = document.querySelector('.people').childElementCount;
 
-peopleprev.addEventListener('click', () => {
-  peoplenext.style.display = "block";
-  let width = 146;
-  if(translatefa === -4416) {
-    peopleprev.style.display = "none";
-  }
-  document.querySelector('.imgandname').style.transform = `translate(${width+translatefa}px)`;
-  translatefa = translatefa + width;
-})
-peoplenext.addEventListener('click', () => {
-  peopleprev.style.display = "block";
-  let width = 146;
-  if(translatefa === -4270-width*(cntfa-1)) {
-    peoplenext.style.display = "none";
-  }
-  document.querySelector('.imgandname').style.transform = `translate(${translatefa-width}px)`;
-  translatefa = translatefa - width;
-})
+// peopleprev.addEventListener('click', () => {
+//   peoplenext.style.display = "block";
+//   let width = 146;
+//   if(translatefa === -4416) {
+//     peopleprev.style.display = "none";
+//   }
+//   document.querySelector('.imgandname').style.transform = `translate(${width+translatefa}px)`;
+//   translatefa = translatefa + width;
+// })
+// peoplenext.addEventListener('click', () => {
+//   peopleprev.style.display = "block";
+//   let width = 146;
+//   if(translatefa === -4270-width*(cntfa-1)) {
+//     peoplenext.style.display = "none";
+//   }
+//   document.querySelector('.imgandname').style.transform = `translate(${translatefa-width}px)`;
+//   translatefa = translatefa - width;
+// })
 
+function clickmsdot(e) {
+  msdot1.style.backgroundColor = '#bbb';
+  msdot2.style.backgroundColor = '#bbb';
+  msdot3.style.backgroundColor = '#bbb';
+  msdot4.style.backgroundColor = '#bbb';
+  msdot5.style.backgroundColor = '#bbb';
+  e.target.style.backgroundColor = '#717171';
+  
+  if(e.target.classList.value === 'ms-dot1'){
+    document.querySelector('.mobile-slider').classList.remove('second-screen');
+    document.querySelector('.mobile-slider').classList.remove('third-screen');
+    document.querySelector('.mobile-slider').classList.remove('fourth-screen');
+    document.querySelector('.mobile-slider').classList.remove('fifth-screen');
+    document.querySelector('.mobile-slider').classList.add('first-screen');
+  }
+  else if(e.target.classList.value === 'ms-dot2'){
+    document.querySelector('.mobile-slider').classList.add('second-screen');
+    document.querySelector('.mobile-slider').classList.remove('third-screen');
+    document.querySelector('.mobile-slider').classList.remove('fourth-screen');
+    document.querySelector('.mobile-slider').classList.remove('fifth-screen');
+    document.querySelector('.mobile-slider').classList.remove('first-screen');
+  }
+  else if(e.target.classList.value === 'ms-dot3'){
+    document.querySelector('.mobile-slider').classList.remove('second-screen');
+    document.querySelector('.mobile-slider').classList.add('third-screen');
+    document.querySelector('.mobile-slider').classList.remove('fourth-screen');
+    document.querySelector('.mobile-slider').classList.remove('fifth-screen');
+    document.querySelector('.mobile-slider').classList.remove('first-screen');
+  }
+  else if(e.target.classList.value === 'ms-dot4'){
+    document.querySelector('.mobile-slider').classList.remove('second-screen');
+    document.querySelector('.mobile-slider').classList.remove('third-screen');
+    document.querySelector('.mobile-slider').classList.add('fourth-screen');
+    document.querySelector('.mobile-slider').classList.remove('fifth-screen');
+    document.querySelector('.mobile-slider').classList.remove('first-screen');
+  }
+  else if(e.target.classList.value === 'ms-dot5'){
+    document.querySelector('.mobile-slider').classList.remove('second-screen');
+    document.querySelector('.mobile-slider').classList.remove('third-screen');
+    document.querySelector('.mobile-slider').classList.remove('fourth-screen');
+    document.querySelector('.mobile-slider').classList.add('fifth-screen');
+    document.querySelector('.mobile-slider').classList.remove('first-screen');
+  }
+}
 function clickmdot(e) {
   mdot2.style.backgroundColor = '#bbb';
   mdot3.style.backgroundColor = '#bbb';
@@ -107,6 +157,11 @@ function clickidot(e) {
   }
 }
 
+msdot1.addEventListener('click', clickmsdot);
+msdot2.addEventListener('click', clickmsdot);
+msdot3.addEventListener('click', clickmsdot);
+msdot4.addEventListener('click', clickmsdot);
+msdot5.addEventListener('click', clickmsdot);
 mdot1.addEventListener('click', clickmdot);
 mdot2.addEventListener('click', clickmdot);
 mdot3.addEventListener('click', clickmdot);
